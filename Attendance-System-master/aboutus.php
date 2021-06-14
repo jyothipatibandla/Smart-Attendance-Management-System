9ccd766384921a5397b47fe0919815f2ac9fcce9
Jyothibhavani patibandla <61370699+jyothipatibandla@users.noreply.github.com>
2021-06-14 17:21:32 +0530

    Add files via upload

---
+++ Attendance-System-master/aboutus.php
@@ -0,0 +1,61 @@
+<html>
+ <head>
+  <link rel="stylesheet" href="css/style.css"/>
+  <title>About Us</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="css/c3.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/highcharts.js"></script>
+  <script src="js/highcharts-exporting.js"></script>
+  <script src="js/jquery.knob.js"></script>
+  <script src="js/student.js"></script>
+  <!-- Custom styles for this template -->
+    <link href="navbar-fixed-top.css" rel="stylesheet">
+ </head>
+ <body>
+ <style>
+ body {
+  background-image: url('img/7.jpg');
+  background-repeat: no-repeat;
+  background-size: 100% 720px;
+}
+h2{
+  text-align: center;
+}
+
+ </style>
+   <!-- Fixed navbar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Online Attendance</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.php">Home</a></li>
+            <li class="active"><a href="#a">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+          
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>
+
+ 
+
+ <h2 style="color:black"><b><i>A Kalyan</i></b> </h2>
+ <h2 style="color:black"><b><i>B Siva Sai</i></b></h2> 
+ <h2 style="color:black"><b><i>K Sasi Kiran</i></b></h2>
+ <h2 style="color:black"><b><i>P Jyothi</i></b></h2>
+ <h2 style="color:black"><b><i>S Thalpa Sai</i></b></h2>
+
+ </body> 
+</html>   
---
+++ Attendance-System-master/contact.php
@@ -0,0 +1,63 @@
+<html>
+ <head>
+  <link rel="stylesheet" href="css/style.css"/>
+  <title>About Us</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="css/c3.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/highcharts.js"></script>
+  <script src="js/highcharts-exporting.js"></script>
+  <script src="js/jquery.knob.js"></script>
+  <script src="js/student.js"></script>
+  <!-- Custom styles for this template -->
+    <link href="navbar-fixed-top.css" rel="stylesheet">
+ </head>
+ <body>
+ <style>
+ body {
+  background-image: url('img/9.jfif');
+  background-repeat: no-repeat;
+  background-size: 100% 720px;
+
+}
+h2{
+  text-align: center;
+}
+h3{
+  text-align: center;
+}
+ </style>
+   <!-- Fixed navbar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">Online Attendance</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="aboutus.php">About</a></li>
+            <li class="active"><a href="#">Contact</a></li>
+          
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav></br></br></br></br></br></br></br></br></br>
+ <h2>Contact Us</h2>
+
+ <h3>A Kalyan ; Email:- kalyansaisanthosh249@gmail.com</h3>
+ <h3>B Shiva Sai ; Email:- bshivasai@gmail.com </h3> 
+ <h3>K Sasi Kiran ; Email:- sasikiran@gmail.com</h3>
+ <h3>P Jyothi ; Email:- jyothi@gmail.com</h3>
+ <h3>S Thalpa ; Email:- thalpasai@gmail.com</h3>
+
+ </body> 
+</html>
